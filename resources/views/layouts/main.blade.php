57202ca479ae25020508b3b5e51d9af1ebbb7e9d
josue <josue1090@gmail.com>
2019-03-23 11:03:50 -0400

    Main layout defined

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="{{ asset('/js/jquery.js') }}"></script>  
+    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+    @stack('scripts')
+    <title>SIS-LAB</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
+    @stack('styles')
+    @yield('header')
+</head>
+<body>
+        @include('partials.navigation') 
+        @include('content')
+        @include('partials.footer')
+</body>    
+    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+</html>
