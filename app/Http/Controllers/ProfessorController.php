be44855ca6da2026dbd91c176f8051ba8d4ca3a0
cr0wg4n <mcm.crw@gmail.com>
2019-03-29 11:09:52 -0400

    error en validations

--- app/Http/Controllers/ProfessorController.php
+++ app/Http/Controllers/ProfessorController.php
@@ -1,21 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Professor;
 class ProfessorController extends Controller
 {
     public function home(){
         return view('components.sections.professorSection');
     }
     public function validations(Request $request){
-        $this -> validate($request,[
-            'names' => 'required',
-            'lastnames' => 'required',
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
-        return $request->all();
-    }
+        $professor= Professor::all();
+        $professor->validations($request);
+    }   
 }
--- app/Professor.php
+++ app/Professor.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Http\Request;
 class Professor extends Model
 {
     public function validations(Request $request){
