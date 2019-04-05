48e418131ed6d24b17dc7860059bdcfb4ac30282
cr0wg4n <mcm.crw@gmail.com>
2019-04-04 22:32:33 -0400

    vista de login correjido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- Custom fonts for this template-->
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <head>
 
-<!-- Custom styles for this template-->
-<link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-<body class="bg-gradient-primary">
+        <title>Ingresar</title>
 
-<div class="container">
+        <!-- Custom fonts for this template-->
+        <link
+            href="vendor/fontawesome-free/css/all.min.css"
+            rel="stylesheet"
+            type="text/css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-        <div class="col-xl-10 col-lg-12 col-md-9">
+    </head>
 
-            <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-            
-                <div class="card o-hidden border-0 shadow-lg my-5"></div>
-                <div class="card-body p-0">
+    <body class="bg-gradient-primary">
 
-                    <!-- Nested Row within Card Body -->
-                    <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+        <div class="container">
 
-                    <div class="col-lg-6">
-                            <div class="p-5">
-                              <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Hola!</h1>
-                    </div>
+            <!-- Outer Row -->
+            <div class="row justify-content-center">
 
-                    <form class="user" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+                <div class="col-xl-10 col-lg-12 col-md-9">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                                <input id="email" type="email" class="form-control form-control-user" name="email" value="{{ old('email') }}" placeholder="Dirección Email" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-
-                                <input id="password" type="password" class="form-control form-control-user" name="password" placeholder="Contraseña" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                        <div class="form-group">
-                            <div class="custom-control custom-checkbox small">
-                                <input class="custom-control-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}>
-                                <label class="custom-control-label" for="customCheck">Recordar</label>
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <!-- Nested Row within Card Body -->
+                            <div class="row">
+                                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Bienvenido Nuevamente</h1>
+                                        </div>
+                                        <form class="user"  role="form" method="POST" action="{{ url('/login') }}">
+                                                {{ csrf_field() }}
+                                            <div class="form-group">
+                                                <input
+                                                    type="email"
+                                                    class="form-control form-control-user"
+                                                    id="email"
+                                                    aria-describedby="emailHelp"
+                                                    name="email"
+                                                    placeholder="Dirección de Correo"
+                                                    value="{{ old('email') }}"
+                                                    required autofocus>
+                                                        @if ($errors->has('email'))
+                                                            <span class="help-block">
+                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                            </span>
+                                                        @endif
+                                            </div>
+                                            <div class="form-group">
+                                                <input
+                                                    type="password"
+                                                    class="form-control form-control-user"
+                                                    id="password"
+                                                    name="password"
+                                                    placeholder="Contraseña"
+                                                    required>
+                                                    @if ($errors->has('password'))
+                                                        <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                        </span>
+                                                    @endif
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox small">
+                                                    <input type="checkbox" class="custom-control-input" id="customCheck" name="remember" {{ old('remember') ? 'checked' : ''}}>
+                                                    <label class="custom-control-label" for="customCheck">Recordar</label>
+                                                </div>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                    Ingresar
+                                            </button>
+                                        </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small" href="{{ url('/password/reset') }}">
+                                                ¿Olvidaste tu Contraseña?
+                                            </a>
+                                        </div>
+                                        <div class="text-center">
+                                            <a class="small" href="{{ url('/register') }}">Crea una Cuenta</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Ingresar
-                                </button>
-                    </form>
-
-                    <hr>
-                    <div class="text-center">
-                            <a class="small" href="{{ url('/password/reset') }}"> Olvidaste tu Contraseña? </a>
-                    </div>
-
-                    <div class="text-center">
-                    <a class="small" href="{{ url('/register') }}">Crea una Cuenta!</a>
                     </div>
 
                 </div>
+
             </div>
+
         </div>
-    </div>
-</div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
+
+    </body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,124 +1,151 @@
-@extends('layouts.app')
-
-@section('content')
-
-<!-- Custom fonts for this template-->
 <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-<!-- Custom styles for this template-->
 <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-<body class="bg-gradient-primary">
+        <body class="bg-gradient-primary">
 
-<div class="container">
+                <div class="container">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
 
-        <div class="card-body p-0">
+                        <div class="card-body p-0">
 
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-                <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                            <div class="row">
+                            
+                                <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
 
+                                    <div class="col-lg-7">
 
-                    <div class="col-lg-7">
-                    <div class="p-5">
+                                        <div class="p-5">
 
-                    <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Registro de Estudiante</h1>
-                    </div>
+                                            <div class="text-center">
+                                                <h1 class="h4 text-gray-900 mb-4">Registro de Estudiante</h1>
+                                            </div>
 
-                    <form class="user" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+                                            <form class="user" role="form" method="POST" action="{{ url('/register') }}">
+                                                {{ csrf_field() }}
 
-                        <div class="form-group row">
-                            <div class="{{ $errors->has('names') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
+                                                <div class="form-group row">
+                                                            <div class="{{ $errors->has('names') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
+                                                                <input
+                                                                    id="names"
+                                                                    type="text"
+                                                                    class="form-control form-control-user"
+                                                                    name="names"
+                                                                    value="{{ old('names') }}"
+                                                                    placeholder="Nombres"
+                                                                    required="required"
+                                                                    autofocus="autofocus">
 
-                                <input id="names" type="text" class="form-control form-control-user" name="names"
-                                       value="{{ old('names') }}" placeholder="Nombres" required autofocus>
+                                                                    @if ($errors->has('names'))
+                                                                    <span class="help-block">
+                                                                        <strong>{{ $errors->first('names') }}</strong>
+                                                                    </span>
+                                                                    @endif
+                                                            </div>
 
-                                @if ($errors->has('names'))
-                                    <span class="help-block">
-                                   <strong>{{ $errors->first('names') }}</strong>
-                               </span>
-                                @endif
-                            </div>
+                                                            <div
+                                                                class="{{ $errors->has('first_name') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
 
-                        <div class="{{ $errors->has('first_name') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
+                                                                <input
+                                                                    id="first_name"
+                                                                    type="text"
+                                                                    class="form-control form-control-user"
+                                                                    name="first_name"
+                                                                    value="{{ old('first_name') }}"
+                                                                    placeholder="Apellido Paterno"
+                                                                    required="required"
+                                                                    autofocus="autofocus">
 
-                            <input id="first_name" type="text" class="form-control form-control-user" name="first_name"
-                                   value="{{ old('first_name') }}" placeholder="Apellido Paterno" required autofocus>
+                                                                    @if ($errors->has('first_name'))
+                                                                    <span class="help-block">
+                                                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                                                    </span>
+                                                                    @endif
+                                                            </div>
 
-                                @if ($errors->has('first_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('first_name') }}</strong>
-                                    </span>
-                                @endif
+                                                            <div
+                                                                class="group{{ $errors->has('second_name') ? ' has-error' : '' }} col-sm-6">
+
+                                                                <input
+                                                                    id="second_name"
+                                                                    type="text"
+                                                                    class="form-control form-control-user"
+                                                                    name="second_name"
+                                                                    value="{{ old('second_name') }}"
+                                                                    placeholder="Apellido Materno"
+                                                                    required="required"
+                                                                    autofocus="autofocus">
+
+                                                                    @if ($errors->has('second_name'))
+                                                                    <span class="help-block">
+                                                                        <strong>{{ $errors->first('second_name') }}</strong>
+                                                                    </span>
+                                                                    @endif
+                                                            </div>
+                                                </div>
+                                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+                                                        <input
+                                                            id="email"
+                                                            type="email"
+                                                            class="form-control form-control-user"
+                                                            name="email"
+                                                            value="{{ old('email') }}"
+                                                            placeholder="Dirección Email"
+                                                            required="required">
+
+                                                            @if ($errors->has('email'))
+                                                            <span class="help-block">
+                                                                <strong>{{ $errors->first('email') }}</strong>
+                                                            </span>
+                                                            @endif
+                                                    </div>
+                                                    <div class="form-group row">
+                                                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
+                                                                <input
+                                                                    id="password"
+                                                                    type="password"
+                                                                    class="form-control form-control-user"
+                                                                    name="password"
+                                                                    placeholder="Contraseña"
+                                                                    required="required">
+
+                                                                @if ($errors->has('password'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                                </span>
+                                                                @endif
+                                                            </div>
+                                                            <div class="col-sm-6">
+                                                                <input
+                                                                    id="password-confirm"
+                                                                    type="password"
+                                                                    class="form-control form-control-user"
+                                                                    name="password_confirmation"
+                                                                    placeholder="Repetir Contraseña"
+                                                                    required="required">
+                                                            </div>
+                                                    </div>
+                                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                        Registrar
+                                                    </button>
+                                            </form>
+                                            <hr>
+                                            <div class="text-center">
+                                                    <a class="small" href="#">¿Olvidaste tu Contraseña?</a>
+                                            </div>
+                                            <div class="text-center">
+                                                    <a class="small" href="{{ url('/login') }}">¿Ya tienes una Cuenta?, Ingresa</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                              </div>
                         </div>
-
-                            <div class="group{{ $errors->has('second_name') ? ' has-error' : '' }} col-sm-6">
-
-                                <input id="second_name" type="text" class="form-control form-control-user"
-                                       name="second_name" value="{{ old('second_name') }}"
-                                       placeholder="Apellido Materno" required autofocus>
-
-                                @if ($errors->has('second_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('second_name') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                    </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                                <input id="email" type="email" class="form-control form-control-user" name="email" value="{{ old('email') }}" placeholder="Dirección Email" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                        <div class="form-group row">\
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-sm-6 mb-3 mb-sm-0">
-
-                                <input id="password" type="password" class="form-control form-control-user" name="password" placeholder="Contraseña" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-
-                        <div class="col-sm-6">
-                                <input id="password-confirm" type="password" class="form-control form-control-user" name="password_confirmation" placeholder="Repetir Contraseña" required>
-                            </div>
-                        </div>
-
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    Registrar
-                                </button>
-                    </form>
-
-                    <hr>
-              <div class="text-center">
-                  <a class="small" href="#">Olvidaste tu Contraseña?</a>
+                  </div>
               </div>
-              <div class="text-center">
-                <a class="small" href="{{ url('/login') }}">Ya tienes una Cuenta? Ingresa!</a>
-              </div>
-
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
-
-@endsection
+                                       
+        </body>
+       
+    
+                            
