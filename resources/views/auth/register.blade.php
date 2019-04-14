44caf8ab96e358c6b0f6d64f9575f95cddca252b
Gustavo <piano7check@gmail.com>
2019-04-14 10:11:53 -0400

    route register stduent was modified

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,10 +36,10 @@
                                         <div class="p-5">
 
                                             <div class="text-center">
-                                                <h1 class="h4 text-gray-900 mb-4">Registro de Estudiante</h1>
+                                                <h1 class="h4 text-gray-900 mb-4">Registro de Estudiante prueba</h1>
                                             </div>
 
-                                            <form class="user" role="form" method="POST" action="{{ url('/register') }}">
+                                            <form class="user" role="form" method="POST" action="{{ url('student/register') }}">
                                                 {{ csrf_field() }}
 
                                                 <div class="form-group row">
