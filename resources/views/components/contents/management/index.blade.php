0db841fe60c506aeff532c30a126448a8fc7502f
cr0wg4n <mcm.crw@gmail.com>
2019-04-30 07:27:43 -0400

    vista de actividades incorporada

--- resources/views/components/contents/management/index.blade.php
+++ resources/views/components/contents/management/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <div class="panel-heading m-0 font-weight-bold text-primary"><strong>{{$title or 'Gestión'}}</strong></div>
+                <div class="panel-heading m-0 font-weight-bold text-primary">{{$title or 'Gestión'}}</div>
                 
                 <div class="card-body">
                     @if (Session::has('status_message'))
