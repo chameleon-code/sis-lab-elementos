4e0b9579207dda6102b174eef897cb140a2dfe97
xen12 <gus.xen.12@gmail.com>
2019-05-10 00:33:02 -0400

    interfaz y funcionalidad para lista de tareas por sesiones de un estudiante

--- resources/views/components/contents/professor/studentList.blade.php
+++ resources/views/components/contents/professor/studentList.blade.php
@@ -1,7 +1,11 @@
-@extends('components.sections.adminSection')
+@extends('components.sections.professorSection')
 @section('userContent')
+<script>
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+</script>
 
-    <script src="/js/generatekey.js"></script>
     <div class="container-fluid">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
@@ -59,9 +63,9 @@
                                             {{-- <td><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{ $item->email }}</font></font></td> --}}
 
                                             <td class="text-center" style="text-align: center; display: flex;">
-                                                <a href="/admin/students/profile/{{ $item->id }}" class="btn btn-info btn-circle btn-sm mx-1"><i title="Ver Perfil" class="fas fa-eye"></i></a>
+                                                <a href="/professor/students/profile/{{ $item->id }}" class="btn btn-info btn-circle btn-sm mx-1" data-toggle="tooltip" title="Ver Perfil"><i class="fas fa-eye"></i></a>
 
-                                                <a href="/admin/students/{{$item->id}}/edit" class="btn btn-warning btn-circle btn-sm mx-1"><i title="Ver Tareas" class="fas fa-edit"></i></a>
+                                                <a href="/professor/studentSesions/{{$item->id}}" class="btn btn-warning btn-circle btn-sm mx-1" data-toggle="tooltip" title="Ver Tareas"><i class="fas fa-edit"></i></a>
                                             </td>
                                         </tr>
                                         @endif
@@ -75,5 +79,4 @@
             </div>
         </div>
     </div>
-
 @endsection
--- resources/views/components/contents/professor/studentSesions.blade.php
+++ resources/views/components/contents/professor/studentSesions.blade.php
@@ -19,23 +19,10 @@
             <div class="card-header py-2">
                 <div class="panel-heading my-2 font-weight-bold text-primary container">
                     Sesiones
-                    <div class="float-right">
-                        <a href="#" class="btn btn-danger btn-icon-split btn-sm" data-toggle="modal" data-target="#deleteSesion">
-                                <span class="icon text-white-50" data-toggle="tooltip" title="Quitar Sesión">
-                                    <i class="fas fa-minus"></i>
-                                </span>
-                            </a>
-                            
-                            <a href="#" class="btn btn-primary btn-icon-split btn-sm" data-toggle="modal" data-target="#addSesion">
-                                <span class="icon text-white-50" data-toggle="tooltip" title="Añadir Sesión">
-                                    <i class="fas fa-plus"></i>
-                                </span>
-                            </a>
-                    </div>
                 </div>
 
                 <div class="container">
-                    <strong>Bloque: </strong> {{ $blockGroup->block_id }}
+                    <strong>Estudiante: </strong> {{ $user->first_name.' '.$user->second_name.' '.$user->names }}
                 </div>
 
                 <div class="card-body">
@@ -57,15 +44,11 @@
                         @foreach ($tasks as $task)
                             @if($task->sesion_id == $sesion->id)
                                 <div class="my-2 mx-2" style="border-bottom: 1px solid #b5b5b5; font-size: 15px;">
-                                        <div style="margin-top: 12px; margin-bottom: -15px;"> <p> <strong> Tarea: </strong> <a href="#">{{ $task->title }}</a> </p> </div>
-                                        <div class="row" style="margin-top: -15px; margin-bottom: -15px;">
+                                        <div style="margin-top: 12px; margin-bottom: -15px;"> <p> <strong> Estado de tarea: </strong> <a href="#">{{ $task->title }}</a> </p> </div>
+                                        <div class="row" style="margin-top: -15px;">
                                             <div class="row" style="margin-left: 12px;">
-                                                <strong> Entregados: </strong>
-                                                <div class="progress progress-sm bg-gray-400" style="margin-top: 8px; margin-left: 15px; margin-right: 10px;">
-                                                    <div class="progress-bar" role="progressbar" style="width: 80px; margin-right: 30px;" aria-valuenow="8" aria-valuemin="0" aria-valuemax="14"></div>
-                                                </div>
+                                                <strong> Entregado: &#10003 &#10005 </strong>
                                             </div>
-                                            <p style="margin-left: 12px;"> 8/14 </p>
                                         </div>
                                         <div> <p> <strong> Límite de entrega: </strong> {{$task->end}} </p> </div>
                                 </div>
@@ -123,6 +106,9 @@
                     </div>
                     </div>
                     
+                    <br>
+                    <a href="/professor/students/list" class="btn btn-primary float-right"> Regresar </a>
+                    <br><br>
         </div>
     </div>
 </div>
