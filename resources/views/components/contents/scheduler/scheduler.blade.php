db41a04b3583ac4eaacf48d211f0bbb8d950db87
Gustavo <piano7check@gmail.com>
2019-05-12 15:14:23 -0400

    scheduler prueba conexion base de datos

--- resources/views/components/contents/scheduler/scheduler.blade.php
+++ resources/views/components/contents/scheduler/scheduler.blade.php
@@ -25,19 +25,55 @@
         <div class="dhx_cal_next_button" style="background-color: #1cc88a;color: #ffffff">&nbsp;</div>
         <div class="dhx_cal_today_button" style="background-color: #4e73df;color: #ffffff"></div>
         <div class="dhx_cal_date"></div>
-        <div class="dhx_cal_tab" name="day_tab"></div>
-        <div class="dhx_cal_tab" name="week_tab"></div>
-        <div class="dhx_cal_tab" name="month_tab"></div>
+        {{--<div class="dhx_cal_tab" name="day_tab"></div>--}}
+        {{--<div class="dhx_cal_tab" name="week_tab"></div>--}}
+        {{--<div class="dhx_cal_tab" name="month_tab"></div>--}}
+        <div>
+            <select class="browser-default custom-select">
+                <option selected>Seleccionar Horario</option>
+                <option value="1">Laboratorio 1</option>
+                <option value="2">Laboratorio 2</option>
+                <option value="3">Laboratorio 3</option>
+                <option value="3">Laboratorio 4</option>
+            </select>
+        </div>
+
     </div>
     <div class="dhx_cal_header"></div>
     <div class="dhx_cal_data"></div>
 </div>
 <script type="text/javascript">
     scheduler.config.xml_date = "%Y-%m-%d %H:%i:%s";
+    //cambiar nombre de columna y especificar solo día
+    scheduler.config.day_date = "%D";
     scheduler.config.first_hour = 7;
     scheduler.config.last_hour = 20;
 
-   // scheduler.config.x_step = 15;
+    //scheduler.config.hour_size_px = 70;
+
+    //prueba header
+    //mygrid = new dhtmlXGridObject('gridbox');
+    //scheduler.setHeader("Sales,Book title,Author,Price");
+    //scheduler.init();
+
+    //Establecer titulo del horario
+    scheduler.templates.week_date = function(start, end){
+        return "Laboratorio 1";
+    };
+
+
+    //intervalo horas
+    scheduler.templates.hour_scale = function(date){
+        if(date.getHours()%1==0)
+            return scheduler.date.date_to_str(scheduler.config.hour_date)(date);
+
+        return '';
+    };
+
+    //intervalo horas efectivo
+
+    //nombre de dias en las clumnas
+
 
     // 0 refers to Sunday, 6 - to Saturday
     scheduler.ignore_week = function(date){
