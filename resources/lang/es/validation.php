a3dffc657c3343e55d1ab1b3df6d9a5da6353288
cr0wg4n <mcm.crw@gmail.com>
2019-05-14 09:16:02 -0400

    carry over gestiones agregado

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -186,8 +186,8 @@ return [
         'managemet'             => 'gestión',
         'management_id'         => 'gestión',
         'subject_matter_id'     => 'materia',
-        'groups_id.*'           => 'grupo'
-
-        
+        'groups_id.*'           => 'grupo',
+        'start_management'      => 'fecha de inicio',
+        'end_management'        => 'fecha de inicio',
     ],
 ];
