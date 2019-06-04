5d16458b852180e73e119652a6626f014c99f12e
cr0wg4n <mcm.crw@gmail.com>
2019-06-04 08:34:41 -0400

    reparando el datepinker

--- resources/views/components/contents/professor/sesions.blade.php
+++ resources/views/components/contents/professor/sesions.blade.php
@@ -1,7 +1,6 @@
 @extends('components.sections.professorSection')
 @section('userContent')
 
-<<<<<<< HEAD
 {{-- <style>
         .accordion-body:after {
             content: '\02228';
@@ -15,20 +14,6 @@
             content: '\02227';
         }
 </style> --}}
-=======
-<style>
-    .accordion-body:after {
-        content: '\02228';
-        color: #777;
-        font-weight: bold;
-        float: right;
-        margin-left: 5px;
-    }
-    .active:after {
-        content: '\02227';
-    }
-</style>
->>>>>>> develop
 
     <div class="container-fluid">
         <div class="card shadow mb-4">
@@ -296,3 +281,6 @@
     <script src="/js/accordion.js"></script>
     <script src="/js/sesions.js"></script>
 @endsection
+@push('scripts')
+  <script src="{{ asset('js/datepicker/datepinker.js') }}"></script>
+@endpush
