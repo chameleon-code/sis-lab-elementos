bb26c978399a1278ce9dc9d21f63dc813ef168ab
xen12 <gus.xen.12@gmail.com>
2019-07-21 18:15:37 -0400

    Ajustando seeders, agregado campo de inscritos y funciones para aumentar y disminuir según la inscripción

--- app/Http/Controllers/StudentScheduleController.php
+++ app/Http/Controllers/StudentScheduleController.php
@@ -53,10 +53,13 @@ class StudentScheduleController extends Controller
 
         $user = Auth::user();
         $student = Student::where('user_id', '=', $user->id)->get()->first();
-        $block_schedule_id = BlockSchedule::find($request->block_schedule_id);
+        $block_schedule = BlockSchedule::find($request->block_schedule_id);
         $group = Group::find($request->group_id);
-        $dir = Block::find($block_schedule_id->block_id)->block_path.'/'.$group->name.'/'.$user->code_sis;//base64_encode($user->code_sis);
+        $dir = Block::find($block_schedule->block_id)->block_path.'/'.$group->name.'/'.$user->code_sis;//base64_encode($user->code_sis);
         
+        $block_schedule->registered++;
+        $block_schedule->save();
+
         StudentSchedule::create([
             'student_id' => $student->id,
             'block_schedule_id' => $request->block_schedule_id,
@@ -89,24 +92,24 @@ class StudentScheduleController extends Controller
     public function edit($id, Request $request)
     {
         $student_schedule = StudentSchedule::findOrFail($id);
-        // $messages = [
-        //     'group_id.required' => 'No puede inscribirse al grupo de la materia seleccionada. ',
-        // ];
-        // $this->validate($request, [
-        //     'group_id' => 'required'
-        // ], $messages);
+        $old_block_schedule = BlockSchedule::findOrFail($student_schedule->block_schedule_id);
+        $old_block_schedule->registered--;
+        $old_block_schedule->save();
 
         $user = Auth::user();
         $student = Student::where('user_id', '=', $user->id)->get()->first();
-        $block_schedule_id = BlockSchedule::find($request->block_schedule_id);
+        $new_block_schedule = BlockSchedule::find($request->block_schedule_id);
         $group = Group::find($request->group_id);
-        $dir = Block::find($block_schedule_id->block_id)->block_path.'/'.$group->name.'/'.base64_encode($user->code_sis);
+        $dir = Block::find($new_block_schedule->block_id)->block_path.'/'.$group->name.'/'.base64_encode($user->code_sis);
         
         $student_schedule->student_id = $student->id;
-        $student_schedule->block_schedule_id = $request->block_schedule_id;
+        $student_schedule->block_schedule_id = $request->block_schedule_id; //nuevo block_schedule
         $student_schedule->group_id = $request->group_id;
         $student_schedule->student_path = $dir;
         $student_schedule->save();
+
+        $new_block_schedule->registered++;
+        $new_block_schedule->save();
         
         Storage::makeDirectory($dir);
 
@@ -134,6 +137,9 @@ class StudentScheduleController extends Controller
     public function destroy($id)
     {
         $student_schedule = StudentSchedule::findOrFail($id);
+        $block_schedule = BlockSchedule::findOrFail($student_schedule->block_schedule_id);
+        $block_schedule->registered--;
+        $block_schedule->save();
         $student_schedule->delete();
 
         return redirect('/students/registration');
