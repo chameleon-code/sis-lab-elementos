586b7c01588b7052beedfea9630462206eba34fd
cr0wg4n <mcm.crw@gmail.com>
2019-07-24 10:44:55 -0400

    cambio que no entiendo

--- public/js/demo/chart-area-demo.js
+++ public/js/demo/chart-area-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun" , "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
