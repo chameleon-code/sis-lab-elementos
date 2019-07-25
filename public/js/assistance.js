0fe4de9083ac9cc2d59be25217a782d92020361d
josue <josue1090@gmail.com>
2019-07-25 15:24:27 -0400

    Ruta y controlador modificados para asistencia

--- public/js/assistance.js
+++ public/js/assistance.js
@@ -1,4 +1,47 @@
-function assistanceRegister(id,bsch_id){
+function checkAssistance(id, bsch_id){
+    console.log('check');
+    $('#student'+id).attr('class', 'btn btn-warning btn-circle btn-sm mx-1');
+    $('#student'+id).attr('title', 'Marcar Asistencia');
+    $('#student'+id).attr('onclick', `assistanceRegister(${id}, ${bsch_id}, 1)`);  
+}
+function uncheckAssistance(id, bsch_id){
+    console.log('uncheck');
+    $('#student'+id).attr('class', 'btn btn-warning btn-circle btn-sm mx-1');
+    $('#student'+id).attr('title', 'Quitar asistencia');
+    $('#student'+id).attr('onclick', `assistanceRegister(${id}, ${bsch_id}, 0)`); 
+}
+function assistanceRegister(id, bsch_id, status){
+    var param = {
+        'blockschedule_id': bsch_id,
+        'student_id': id,
+        'status': status
+    };
+    $.ajax({
+        url : window.location.origin+'/auxiliar/assistance',
+        type: 'POST',
+        headers: {
+            'x-csrf-token': $("meta[name=csrf-token]").attr('content')
+        },
+        data: param,
+        success: (res) => {
+            if(res.res) {
+                if(status == 1){
+                    //checkAssistance(param['student_id'], param['blockschedule_id']);  
+                    $('#dataTable').DataTable().ajax.reload();                  
+                }   
+                else{
+                    //uncheckAssistance(param['student_id'], param['blockschedule_id']);
+                    $('#dataTable').DataTable().ajax.reload();
+                }           
+            } else {
+                Swal.fire({
+                    type: 'error',
+                    title: 'Algo salio mal...',
+                    text: 'No se pudo guardar la informacion, vuelve a intentarlo',
+                  })
+            }
+        }
+    });
     $('#student_id').val(id);
     $('#blockschedule_id').val(bsch_id);
 }
@@ -8,71 +51,70 @@ $(document).ready(()=>{
         var array = Array();
         var table = $('#dataTable').DataTable();
         $.ajax({
-            url: window.location.origin+`auxiliar/getStudentList/${event.target.value}`,
+            url: window.location.origin+`/auxiliar/getStudentList/${event.target.value}`,
             success: (response)=>{
                 array = response;
             }
         })
         .then(()=>{
             table.destroy(); 
-            console.log(array)
+            console.log(array);
             table = $('#dataTable').DataTable({
                 data: array,
-                buttons:'selectCells',
                 columns: [
                     { data: 'Codigo_Sis'},
                     { data: 'Apellidos'},
                     { data: 'Nombres'},
                     { data: 'Asistencia',
-                    render : function(data, type, row) {
-                        if(data.assist === true){
-                            return `<a href="#" class="btn btn-warning btn-circle btn-sm mx-1" data-toggle-2="tooltip" title="Marcar Asistencia" data-toggle="modal" data-target="#appModal" onclick="assistanceRegister(${data.student}, ${data.bsch_id})" id="student${data.student}"><i class="far fa-check-square"></i></a>`
-                        }
-                        else{
-                            return `<a href="#" class="btn btn-success btn-circle btn-sm mx-1" data-toggle-2="tooltip" title="Estudiante registrado" data-toggle="modal" id="student${data.student}"><i class="far fa-check-square"></i></a>`
-                        }
-                    } 
+                        render: function(data, type, row) {
+                            if(data.assist === true){
+                                return `<a href="#" class="btn btn-warning btn-circle btn-sm mx-1" data-toggle-2="tooltip" title="Marcar Asistencia" onclick="assistanceRegister(${data.student}, ${data.bsch_id}, 1)" id="${data.student}"><i class="far fa-check-square"></i></a>`
+                            }
+                            else{
+                                return `<a href="#" class="btn btn-success btn-circle btn-sm mx-1" data-toggle-2="tooltip" title="Quitar asistencia" onclick="assistanceRegister(${data.student}, ${data.bsch_id}, 0)" id="${data.student}"><i class="far fa-check-square"></i></a>`
+                            }
+                        } 
                     }                      
                 ]
             }).draw();
         });
     });
-    $('#modalAction').click(function(event){
-            console.log($("#registerEvent").serialize());
-            $.ajax({
-                url : window.location.origin+'auxiliar/assistance',
-                type: 'POST',
-                headers: {
-                    'x-csrf-token': $("meta[name=csrf-token]").attr('content')
-                },
-                data: {
-                    info: $("#registerStudent").serialize()
-                },
-                success: (res) => {
-                    if(res.res) {
-                       Swal.fire(
-                            'Registro de estudiante exitoso!',
-                            'Revisa en tu registro de asistencia',
-                            'success'
-                        );
-                        modal.modal('hide');        
-                    } else {
-                        Swal.fire({
-                            type: 'error',
-                            title: 'Algo salio mal...',
-                            text: 'No se pudo guardar la informacion, vuelve a intentarlo',
-                          })
-                    }
-                }
-            });
-        });
-    $('#modalAction').click( ()=>{
-        var modal = $('#appModal');
-        var id = modal.find('#student_id').val();
-        $('#student'+id).attr('class', 'btn btn-success btn-circle btn-sm mx-1');
-        $('#student'+id).attr('title', 'Estudiante registrado');
-        $('#student'+id).attr('data-target', '');
-        $('#student'+id).attr('onclick', '');         
-    });
+    // $('#modalAction').click(function(event){
+    //         console.log($("#registerEvent").serialize());
+    //         $.ajax({
+    //             url : window.location.origin+'/auxiliar/assistance',
+    //             type: 'POST',
+    //             headers: {
+    //                 'x-csrf-token': $("meta[name=csrf-token]").attr('content')
+    //             },
+    //             data: {
+    //                 info: $("#registerStudent").serialize()
+    //             },
+    //             success: (res) => {
+    //                 if(res.res) {
+    //                    Swal.fire(
+    //                         'Registro de estudiante exitoso!',
+    //                         'Revisa en tu registro de asistencia',
+    //                         'success'
+    //                     );
+    //                     modal.modal('hide');        
+    //                 } else {
+    //                     Swal.fire({
+    //                         type: 'error',
+    //                         title: 'Algo salio mal...',
+    //                         text: 'No se pudo guardar la informacion, vuelve a intentarlo',
+    //                       })
+    //                 }
+    //             }
+    //         });
+    //     });
+    // $('#modalAction').click( ()=>{
+    //     var modal = $('#appModal');
+    //     var id = modal.find('#student_id').val();
+    //     $('#student'+id).attr('class', 'btn btn-success btn-circle btn-sm mx-1');
+    //     $('#student'+id).attr('title', 'Estudiante registrado');
+    //     $('#student'+id).attr('data-target', '');
+    //     $('#student'+id).attr('onclick', '');         
+    // });
 });
 
--- resources/views/components/contents/professor/registerAssistance.blade.php
+++ resources/views/components/contents/professor/registerAssistance.blade.php
@@ -53,9 +53,9 @@
                                                 <td class="mgx-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{ $item->user->code_sis }}</font></font></td>
                                                 @forelse ($sesions as $sesion)
                                                     @if (in_array($sesion->id ,array_pluck($item->student->assistances->toArray(), 'sesion_id')))
-                                                        <td><a href="#" class="btn btn-success btn-circle btn-sm mx-1" ><i class="far fa-check-square"></i></a></td>
+                                                        <td><a href="#" class="btn btn-success btn-circle btn-sm mx-1" onclick="assistanceRegister({{ $item->student->id }}, {{ $item->id }}, 0)"><i class="far fa-check-square"></i></a></td>
                                                     @else  
-                                                        <td ><a href="#" class="btn btn-danger btn-circle btn-sm mx-1" ><i class="fas fa-times"></i></a></td>    
+                                                        <td ><a href="#" class="btn btn-danger btn-circle btn-sm mx-1" onclick="assistanceRegister({{ $item->student->id }}, {{ $item->id }}, 1)"><i class="fas fa-times"></i></a></td>    
                                                     @endif                                                  
                                                 @empty                                                    
                                                 @endforelse
@@ -70,34 +70,6 @@
             </div>
         </div>
     </div>
-
-    <div class="modal fade" id="appModal" tabindex="-1" role="dialog" aria-labelledby="appModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="appModalLabel">Confirmacion de Asistencia</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="" id="registerStudent">
-                        <p>Desea confirmar la asistencia del alumno? </p>
-                        <input name="student_id" type="hidden" value="" id="student_id">
-                        <input name="blockschedule_id" type="hidden" value="" id="blockschedule_id">
-                    </form>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary" id="modalAction">Confirmar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
 @endsection
 
 @push('scripts')
