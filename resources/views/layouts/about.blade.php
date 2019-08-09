841f4134d08f54307539047359f6162c8871fe8c
cr0wg4n <mcm.crw@gmail.com>
2019-08-09 02:58:19 -0400

    about agregado

---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Equipo Chameleon Code</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.css" rel="stylesheet">
+
+</head>
+
+<body>
+ 
+    
+    <section class="testimonials text-center bg-light">
+        <div class="container">
+        <a href="javascript:history.go(-1)"><img src="/img/chameleon.png" alt="" width="55%"></a> 
+            <br>
+            <br>
+            <p class="lead mb-0">Este sistema ha sido desarrollado en la materia de <strong>Taller de Ingeniería de Software</strong></p>
+            <br>
+            <h2 class="mb-5">Nuestro Equipo</h2>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="/img/broster.jpg" alt="">
+                        <h5>Josue Flores R.</h5>
+                        <p class="font-weight-light mb-0"><a href="https://github.com/josue1090">josue1090</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="/img/broster.jpg" alt="">
+                            <h5>Gustavo Patiño L.</h5>
+                            <p class="font-weight-light mb-0"><a href="https://github.com/xen12">xen12</a></p>
+                        </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="/img/mauri.png" alt="">
+                        <h5>Mauricio Matias C.</h5>
+                        <p class="font-weight-light mb-0"><a href="https://github.com/cr0wg4n">cr0wg4n</a></p>
+                    </div>
+                </div>
+                
+                <div class="col-lg-6">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="/img/steve.jpg" alt="">
+                        <h5>Gustavo Rojas V.</h5>
+                        <p class="font-weight-light mb-0"><a href="https://github.com/Gustavo-RV">Gustavo-RV</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img class="img-fluid rounded-circle mb-3" src="/img/deni.jpg" alt="">
+                        <h5>Denikin Santos U.</h5>
+                        <p class="font-weight-light mb-0"><a href="https://github.com/denikin007">denikin007</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="showcase">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+        
+                <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/img/message2.png');"></div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                    <h2>Reportar Incidentes</h2>
+                    <br>
+                    <p class="lead mb-0">Cualquier problema con el sistema debe ser informado inmediatamente al correo:</p><br>
+                    <h6><a href="mailto:chameleon.code.soft@gmail.com">chameleon.code.soft@gmail.com</a></h6>
+                </div>
+                </div>
+            </div>
+    </section>
+  
+    <footer class="footer bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                    <ul class="list-inline mb-2">
+                        <li class="list-inline-item">
+                            <a href="javascript:history.go(-1)">Volver</a>
+                        </li>
+                        <li class="list-inline-item">
+                            |
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="/about"  title="Si tienes alguna queja u observación, haz click aquí">Contactanos</a>
+                        </li>
+                    </ul>
+                <p class="text-muted small mb-4 mb-lg-0">Copyright &copy; Chameleon Code 2019</p>
+                </div>
+                <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mr-3">
+                    <a href="https://github.com/chameleon-code/">
+                        <i class="fab fa-github fa-2x fa-fw"></i>
+                    </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
